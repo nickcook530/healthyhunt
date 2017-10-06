aa0c1025a968c78e1d801372d6550eebb26ad3d8
Nicholas Cook <nickcook530@gmail.com>
2017-10-06 11:24:58 -0400

    added headers, improved buttons and general visibility

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
@@ -48,11 +49,18 @@ class RestaurantScreen(Screen):
         self.restaurant_screen_layout()
 
     def restaurant_screen_layout(self):
-        layout = GridLayout(cols=1, spacing=10, size_hint_y=None, padding=[50, 0, 50, 0]) # size_hint_x=.8
-        root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height), bar_color=(0.051, 0.173, 1, 0.9),
-                          bar_width=10)
+        box = BoxLayout(orientation='vertical')
+        header = Label(text='1) Select a Restaurant:', font_size='50', halign='left', padding=(100, 100), markup=True, bold=True)
+        box.add_widget(header)
+        layout = GridLayout(cols=1, spacing=10, size_hint_y=None, padding=[50, 0, 50, 0])  # size_hint_x=.8
+
+        #box.add_widget(header)
+        scroll = ScrollView(size_hint=(1, None), size=(Window.width, Window.height-header.height), bar_color=(0.051, 0.173, 1, 0.9),
+                            bar_inactive_color=(0.051, 0.173, 1, 0.9), bar_width=10)
+
         # Make sure the height is such that there is something to scroll.
         layout.bind(minimum_height=layout.setter('height'))
+
         with requests.Session() as s:
             query_dict = s.get('http://www.healthyhunt.xyz/restaurantlist',
                                auth=HTTPBasicAuth('healthykivy', 'knockknock469')).json()
@@ -66,8 +74,9 @@ class RestaurantScreen(Screen):
             rank += 1
         # layer on the widgets to allow scrolling
 
-        root.add_widget(layout)
-        self.add_widget(root)
+        scroll.add_widget(layout)
+        box.add_widget(scroll)
+        self.add_widget(box)
 
 class FilterScreen(Screen):
     def __init__(self, *args, **kwargs):
@@ -76,8 +85,10 @@ class FilterScreen(Screen):
         self.filter_screen_layout()
 
     def filter_screen_layout(self):
-        #layout = GridLayout(cols=1, spacing=10, padding=[50, 0, 50, 0])
         layout = BoxLayout(orientation='vertical', spacing=10, padding=[50, 0, 50, 0])
+        header = Label(text='2) Select a Nutrition Filter:', font_size='50', halign='left', padding=(100, 100), markup=True, bold=True)
+        layout.add_widget(header)
+
         lowcal_btn = Button(text='Low Calorie Options')
         lowcal_btn.bind(on_release=filter_to_result)
         layout.add_widget(lowcal_btn)
@@ -100,8 +111,12 @@ class ResultScreen(Screen):
 
     def result_screen_layout(self):
         global final_url
-        root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height), bar_color=(0.051, 0.173, 1, 0.9),
-                          bar_width=10)
+
+        box = BoxLayout(orientation='vertical')
+        header = Label(text='3) Tasty Results!', font_size='50', halign='left', padding=(100, 100), markup=True, bold=True)
+        box.add_widget(header)
+        scroll = ScrollView(size_hint=(1, None), size=(Window.width, Window.height-header.height), bar_color=(0.051, 0.173, 1, 0.9),
+                          bar_inactive_color=(0.051, 0.173, 1, 0.9), bar_width=10)
         layout = GridLayout(cols=1, spacing=10, size_hint_y=None, padding=[50, 0, 50, 0])
         # Make sure the height is such that there is something to scroll.
         layout.bind(minimum_height=layout.setter('height'))
@@ -110,15 +125,45 @@ class ResultScreen(Screen):
             s.close()
 
         rank = 1
-        for _ in query_dict:
-            btn = Button(text=str(query_dict[str(rank)]['name']) + ' | calories = {},'.format(query_dict[str(rank)]['calories']),
-                         size_hint_y=None, height=150, text_size=(None,None))
-            btn.bind(on_release=restart)
-            layout.add_widget(btn)
-            rank += 1
 
-        root.add_widget(layout)
-        self.add_widget(root)
+        if 'lowcal' in final_url:
+            for _ in query_dict:
+                name= query_dict[str(rank)]['name']
+                calories = query_dict[str(rank)]['calories']
+                btn = Button(text=str(name) + '\nCalories = {}'.format(calories),
+                                    size_hint_y=None, height=150, halign='center', text_size=(None,None))
+                btn.bind(on_release=restart)
+                layout.add_widget(btn)
+                rank += 1
+
+        if 'lowcarb' in final_url:
+            for _ in query_dict:
+                name= query_dict[str(rank)]['name']
+                carbs = query_dict[str(rank)]['carbs']
+                calories = query_dict[str(rank)]['calories']
+                btn = Button(text=str(name) + '\nCarbs = {} grams'.format(carbs) +
+                                ' | Calories = {}'.format(calories), size_hint_y=None, height=150, halign='center',
+                                text_size=(None,None))
+                btn.bind(on_release=restart)
+                layout.add_widget(btn)
+                rank += 1
+
+        if 'highprotein' in final_url:
+            for _ in query_dict:
+                name= query_dict[str(rank)]['name']
+                protein = query_dict[str(rank)]['protein']
+                calories = query_dict[str(rank)]['calories']
+                ratio = round(protein/calories, 2)
+                btn = Button(text=str(name) + '\nProtein = {} grams,'.format(protein) +
+                              ' | Calories = {}'.format(calories) + '\nRatio = {} grams of Protein per Calorie'.format(ratio),
+                             size_hint_y=None, height=200, halign='center', text_size=(None,None))
+                btn.bind(on_release=restart)
+                layout.add_widget(btn)
+                rank += 1
+
+        scroll.add_widget(layout)
+        box.add_widget(scroll)
+        self.add_widget(box)
 
 class MyScreenManagement(ScreenManager):
     def __init__(self, *args, **kwargs):
@@ -129,10 +174,8 @@ class MyScreenManagement(ScreenManager):
     def boot_up(self):
         sc1 = RestaurantScreen()
         sc2 = FilterScreen()
-        #sc3 = ResultScreen()
         self.add_widget(sc1)
         self.add_widget(sc2)
-        #self.add_widget(sc3)
 
 
 
